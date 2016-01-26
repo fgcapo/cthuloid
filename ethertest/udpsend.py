219aa402c781152980b8dc5de0380ab7764ed158
Francesco Capodieci <fgcapo@gmail.com>
2016-01-25 22:23:30 -0800

    udp test scripts

---
+++ ethertest/udpsend.py
@@ -0,0 +1,21 @@
+import socket
+import time
+
+#UDP_IP = '192.168.1.70'
+UDP_IP = '10.0.0.70'
+UDP_PORT = 1337
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+print "UDP target IP:", UDP_IP
+print "UDP target port:", UDP_PORT, "\n"
+
+def sendCmd(cmd):
+  print "Sending '", cmd, "'"
+  sock.sendto(cmd + "\n", (UDP_IP, UDP_PORT))
+  print "Awaiting echo..."
+  print sock.recvfrom(1024), "\n"
+
+sendCmd("p 4")
+time.sleep(1)
+sendCmd("p 3")
+
